3593c2b3de1f70054958dbe26a227f5d4084cbb1
ading2016 <934588176@qq.com>
2016-05-19 11:09:35 +0800

    add class StoryHandle

--- My_Tornado/My_First_Tornado.py
+++ My_Tornado/My_First_Tornado.py
@@ -5,9 +5,13 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+class StoryHandle(tornado.web.RequestHandler):
+    def get(self, story_id):
+        self.write("You requested the story " + story_id)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/story/([0-9]+)",StoryHandle),
 ])
 
 if __name__ == "__main__":
