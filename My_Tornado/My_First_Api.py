97aa9cc943ae326e2667a6b76181a23524b81b25
ading2016 <934588176@qq.com>
2016-05-19 17:11:45 +0800

    change class UpdateGit

--- My_Tornado/My_First_Api.py
+++ My_Tornado/My_First_Api.py
@@ -1,11 +1,13 @@
 __author__ = 'Administrator'
 import os
 import sys
+import time
 import My_First_Tornado
 class UpdateGit():
     def get(self):
         self.message = My_First_Tornado.MainHandler.get_argument("message")
-        while true:
+        while 1:
             print self.message
+            time.sleep(5)
 
 
