eb3edb535f1608bbd3776b063c27fabc0cfea2e2
ading2016 <934588176@qq.com>
2016-05-19 12:07:42 +0800

    change class MainHandle

--- My_Tornado/My_First_Tornado.py
+++ My_Tornado/My_First_Tornado.py
@@ -4,7 +4,14 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write('<html><body><form action="/" method="post">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+    def post(self):
+        self.set_header("Content-Type","text/plain")
+        self.write("You wrote " + self.get_argument("message"))
+
 class StoryHandle(tornado.web.RequestHandler):
     def get(self, story_id):
         self.write("You requested the story " + story_id)
