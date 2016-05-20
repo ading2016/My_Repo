871043f6169a574a1d85b5d33667699fd4e62f56
ading2016 <934588176@qq.com>
2016-05-20 12:41:26 +0800

    +class LoginHandler(BaseHandler):

--- My_Tornado/My_First_Tornado.py
+++ My_Tornado/My_First_Tornado.py
@@ -44,6 +44,28 @@ class ProfileHandle(tornado.web.RequestHandler):
         self.database = database
     def get(self,username):
         self.username = username
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.write("Hello, " + name)
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body><form action="/login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
 
 
 application = tornado.web.Application([
@@ -51,7 +73,8 @@ application = tornado.web.Application([
     (r"/story/([0-9]+)",StoryHandle),
     (r"/file",UploadFile),
     (r"/user",ProfileHandle),
-])
+    (r"/login", LoginHandler),
+], cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=")
 
 if __name__ == "__main__":
     application.listen(8888)
